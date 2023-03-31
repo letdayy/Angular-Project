5e0ff9a3199b4ae96921f1ade76c9b0afb072b3f
letdayy <lcdayane49@gmail.com>
2023-03-31 19:41:00 -0300

    conceitos de data binding aplicados

--- exercicio3/src/app/app.component.ts
+++ exercicio3/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'exercicio3';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  titulo = "Listagem de Colaboradores";
 }
--- exercicio3/src/app/features/colaboradores/listagem/listagem.component.ts
+++ exercicio3/src/app/features/colaboradores/listagem/listagem.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-listagem',
   templateUrl: './listagem.component.html',
   styleUrls: ['./listagem.component.scss']
 })
-export class ListagemComponent {
+export class ListagemComponent implements OnInit {
 
+
+  colaborador = {
+    id: 1,
+    imagem: "https://www.freeiconspng.com/thumbs/person-icon/clipart--person-icon--cliparts-15.png",
+    nome: "Nathan Carlos Exercicio",
+    salario: 4500,
+    cargo: "Dev PI"
+  };
+
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    console.log("teste");
+  }
 }
